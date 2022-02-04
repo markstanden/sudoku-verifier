4c76168615ac81cd33594c465a70a1e9e8a86b35
Mark Standen <mark@markstanden.dev>
2022-02-04 17:22:37 +0000

    neatened up

--- src/main/java/SudokuVerifier.java
+++ src/main/java/SudokuVerifier.java
@@ -23,7 +23,7 @@ public class SudokuVerifier {
      * To the console.
      */
     public void verifySolution() {
-        System.out.println("Solution is ".concat(isValid() ? "Valid" : "Invalid"));
+        System.out.println("Solution is ".concat(gridIsValid() ? "Valid" : "Invalid"));
     }
 
     /**
@@ -31,25 +31,15 @@ public class SudokuVerifier {
      *
      * @return returns true if the supplied grid is valid, false if invalid
      */
-    private boolean isValid() {
-        return rowsAreValid()
+    private boolean gridIsValid() {
+        return  rowsAreValid()
                 && columnsAreValid()
                 && blocksAreValid();
     }
 
     /**
-     * Checks all rows for validity
-     *
-     * @return true if valid, false if invalid
-     */
-    private boolean rowsAreValid() {
-        return Stream.of(grid).parallel()                   // Stream<int[]>
-                .map(this::arrayToSet)                      // Stream<Set<Integer>>
-                .allMatch(this::isValidSet);                // Boolean
-    }
-
-    /**
-     * Converts an array into an unordered set and returns it.
+     * The workhorse of the verifier, it converts an
+     * array into an unordered set and returns it.
      *
      * @param array The array to convert to a set.
      * @return An unordered set of the contents of the array.
@@ -61,21 +51,48 @@ public class SudokuVerifier {
                 .collect(Collectors.toSet());               // Set<Integer>
     }
 
-    private boolean columnsAreValid() {
-        return IntStream.range(0, grid[0].length)           // IntStream
-                .parallel()                                 // IntStream
-                .mapToObj(index -> Arrays.stream(grid)      // Stream<int[]>
-                        .parallel()                         // Stream<int[]>
-                        .mapToInt(row -> row[index])        // IntStream
-                        .boxed()                            // Stream<Integer>
-                        .collect(Collectors.toSet())        // Set<Integer>
-                )                                           // Stream<Set<Integer>>
+    /**
+     * Validates that the group being tested is valid.
+     *
+     * @param set the set to be tested, whether a row, column or block.
+     * @return true if valid, false if invalid.
+     */
+    private boolean isValidSet(Set<Integer> set) {
+        return set.containsAll(arrayToSet(VALID_NUMBERS));
+    }
+
+    /**
+     * Checks all rows for validity.
+     *
+     * @return true if valid, false if invalid.
+     */
+    private boolean rowsAreValid() {
+        return Stream.of(grid).parallel()                   // Stream<int[]>
+                .map(this::arrayToSet)                      // Stream<Set<Integer>>
                 .allMatch(this::isValidSet);                // Boolean
     }
 
     /**
-     * Checks each 3x3 Block for validity
-     * @return  true if all blocks are valid
+     * Checks all columns for validity.
+     *
+     * @return true if all blocks are valid.
+     */
+    private boolean columnsAreValid() {
+        return IntStream.range(0, grid[0].length)           // IntStream                Produce indices from 0-8
+                .parallel()                                 // IntStream                Many indices at once
+                .mapToObj(index -> Arrays.stream(grid)          // Stream<int[]>            Stream of rows
+                        .parallel()                             // Stream<int[]>            Many rows at once
+                        .mapToInt(row -> row[index])            // IntStream                values at current index
+                        .boxed()                                // Stream<Integer>          toSet won't autobox from an IntStream
+                        .collect(Collectors.toSet())            // Set<Integer>             Add all the values into a set
+                )                                           // Stream<Set<Integer>>     Stream of Sets
+                .allMatch(this::isValidSet);                // Boolean                  Validate all the produced sets, only true if All true
+    }
+
+    /**
+     * Checks each 3x3 Block for validity.
+     *
+     * @return  true if all blocks are valid.
      */
     private boolean blocksAreValid() {
 
@@ -107,10 +124,5 @@ public class SudokuVerifier {
             }
         }
         return true;
-    }
-
-    private boolean isValidSet(Set<Integer> group) {
-        //return group.size() == 9;
-        return group.containsAll(arrayToSet(VALID_NUMBERS));
     }
 }
