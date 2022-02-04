c67e8919eaadaaf1094902c3fdee5e277983f4fa
Mark Standen <mark@markstanden.dev>
2022-02-04 17:30:35 +0000

    removed wildcard imports

--- src/main/java/SudokuVerifier.java
+++ src/main/java/SudokuVerifier.java
@@ -1,4 +1,6 @@
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
