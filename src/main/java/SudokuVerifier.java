076572e5bc6c587f1f2891dec8b315921fe85f26
Mark Standen <mark@markstanden.dev>
2022-02-04 16:44:39 +0000

    all tests pass

--- src/main/java/SudokuVerifier.java
+++ src/main/java/SudokuVerifier.java
@@ -1,24 +1,33 @@
-import java.util.Arrays;
-import java.util.Set;
-import java.util.function.Function;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class SudokuVerifier {
     public static final int[] VALID_NUMBERS = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    public static final int BLOCK_SIZE = 3;
+
     private final int[][] grid;
 
     SudokuVerifier(final int[][] grid) {
         this.grid = grid;
     }
 
+    /**
+     * Prints a message to the console informing
+     * the user whether the grid is valid or not.
+     *
+     * Prints either:
+     * Solution is Valid
+     * Solution is Invalid
+     * To the console.
+     */
     public void verifySolution() {
         System.out.println("Solution is ".concat(isValid() ? "Valid" : "Invalid"));
     }
 
     /**
-     * Checks whether the grid is valid
+     * Checks whether the grid is valid.
      *
      * @return returns true if the supplied grid is valid, false if invalid
      */
@@ -39,6 +48,12 @@ public class SudokuVerifier {
                 .allMatch(this::isValidSet);                // Boolean
     }
 
+    /**
+     * Converts an array into an unordered set and returns it.
+     *
+     * @param array The array to convert to a set.
+     * @return An unordered set of the contents of the array.
+     */
     private Set<Integer> arrayToSet(final int[] array) {
         return Arrays.stream(array)                         // IntStream
                 .parallel()                                 // IntStream
@@ -55,14 +70,47 @@ public class SudokuVerifier {
                         .boxed()                            // Stream<Integer>
                         .collect(Collectors.toSet())        // Set<Integer>
                 )                                           // Stream<Set<Integer>>
-                .allMatch(this::isValidSet);                  // Boolean
+                .allMatch(this::isValidSet);                // Boolean
     }
 
+    /**
+     * Checks each 3x3 Block for validity
+     * @return  true if all blocks are valid
+     */
     private boolean blocksAreValid() {
+
+        // Checks in columns:
+        //      A D G
+        //      B E H
+        //      C F I
+
+        for(int startCol = 0; startCol <= grid[0].length - BLOCK_SIZE; startCol = startCol + BLOCK_SIZE){
+            // Break the grid into columns
+
+            Set<Integer> blockSet = new HashSet<>();
+            for(int row = 0; row <= grid.length - BLOCK_SIZE; row++){
+                // left-hand column of each of the three columns
+                // iterate each row
+
+                // Create a copy of the current block line and add to the block set
+                Set<Integer> currentLine = arrayToSet(Arrays.copyOfRange(grid[row], startCol, startCol + BLOCK_SIZE));
+                blockSet.addAll(currentLine);
+
+                // if the next row index is a multiple of three
+                // we have finished the block, so check the set
+                if((row + 1) % 3 == 0) {
+                    // if block is not valid, exit early
+                    if(!isValidSet(blockSet)){
+                        return false;
+                    }
+                }
+            }
+        }
         return true;
     }
 
     private boolean isValidSet(Set<Integer> group) {
-        return group.size() == 9;
+        //return group.size() == 9;
+        return group.containsAll(arrayToSet(VALID_NUMBERS));
     }
 }
--- src/test/java/SudokuVerifierTest.java
+++ src/test/java/SudokuVerifierTest.java
@@ -4,67 +4,59 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SudokuVerifierTest {
 
-    public static final int [][] VALID = {  {2,4,1, 6,9,5,  3,8,7},
-                                            {7,3,5, 4,2,8,  1,6,9},
-                                            {8,6,9, 7,3,1,  4,2,5},
+    public static final int[][] VALID = {   {2, 4, 1, 6, 9, 5, 3, 8, 7},
+                                            {7, 3, 5, 4, 2, 8, 1, 6, 9},
+                                            {8, 6, 9, 7, 3, 1, 4, 2, 5},
+                                            {4, 1, 3, 8, 7, 9, 2, 5, 6},
+                                            {6, 9, 2, 5, 1, 3, 7, 4, 8},
+                                            {5, 8, 7, 2, 4, 6, 9, 3, 1},
+                                            {1, 7, 8, 3, 6, 4, 5, 9, 2},
+                                            {9, 5, 4, 1, 8, 2, 6, 7, 3},
+                                            {3, 2, 6, 9, 5, 7, 8, 1, 4}};
 
-                                            {4,1,3, 8,7,9,  2,5,6},
-                                            {6,9,2, 5,1,3,  7,4,8},
-                                            {5,8,7, 2,4,6,  9,3,1},
+    public static final int[][] INVALID = { {2, 4, 1, 6, 9, 5, 3, 8, 1},
+                                            {7, 3, 5, 4, 2, 8, 1, 6, 9},
+                                            {8, 6, 9, 7, 3, 1, 4, 2, 5},
+                                            {4, 1, 3, 8, 7, 9, 2, 5, 6},
+                                            {6, 9, 2, 5, 2, 3, 7, 4, 8},
+                                            {5, 8, 7, 2, 4, 6, 9, 3, 1},
+                                            {1, 7, 8, 3, 6, 4, 5, 9, 2},
+                                            {8, 5, 4, 1, 8, 2, 6, 7, 3},
+                                            {3, 2, 6, 9, 5, 7, 8, 1, 4}};
 
-                                            {1,7,8, 3,6,4,  5,9,2},
-                                            {9,5,4, 1,8,2,  6,7,3},
-                                            {3,2,6, 9,5,7,  8,1,4}};
+    public static final int[][] INVALID_COLS = {{1, 2, 3, 4, 5, 6, 7, 8, 9},
+                                                {4, 5, 6, 7, 8, 9, 1, 2, 3},
+                                                {8, 6, 9, 1, 2, 3, 4, 5, 6},
+                                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                                                {4, 5, 6, 7, 8, 9, 1, 2, 3},
+                                                {8, 6, 9, 1, 2, 3, 4, 5, 6},
+                                                {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                                                {4, 5, 6, 7, 8, 9, 1, 2, 3},
+                                                {8, 6, 9, 1, 2, 3, 4, 5, 6}};
 
-    public static final int [][] INVALID = {{2,4,1, 6,9,5,  3,8,1},
-                                            {7,3,5, 4,2,8,  1,6,9},
-                                            {8,6,9, 7,3,1,  4,2,5},
-                                            {4,1,3, 8,7,9,  2,5,6},
-                                            {6,9,2, 5,2,3,  7,4,8},
-                                            {5,8,7, 2,4,6,  9,3,1},
-                                            {1,7,8, 3,6,4,  5,9,2},
-                                            {8,5,4, 1,8,2,  6,7,3},
-                                            {3,2,6, 9,5,7,  8,1,4}};
+    public static final int[][] INVALID_ROWS = {{1, 4, 7, 1, 4, 7, 1, 4, 7},
+                                                {2, 5, 8, 2, 5, 8, 2, 5, 8},
+                                                {3, 6, 9, 3, 6, 9, 3, 6, 9},
+                                                {4, 7, 1, 4, 7, 1, 4, 7, 1},
+                                                {5, 8, 2, 5, 8, 2, 5, 8, 2},
+                                                {6, 9, 3, 6, 9, 3, 6, 9, 3},
+                                                {7, 1, 4, 7, 1, 4, 7, 1, 4},
+                                                {8, 2, 5, 8, 2, 5, 8, 2, 5},
+                                                {9, 3, 6, 9, 3, 6, 9, 3, 6}};
 
-    public static final int [][] INVALID_COLS = {   {1,2,3, 4,5,6,  7,8,9},
-                                                    {4,5,6, 7,8,9,  1,2,3},
-                                                    {8,6,9, 1,2,3,  4,5,6},
-
-                                                    {1,2,3, 4,5,6,  7,8,9},
-                                                    {4,5,6, 7,8,9,  1,2,3},
-                                                    {8,6,9, 1,2,3,  4,5,6},
-
-                                                    {1,2,3, 4,5,6,  7,8,9},
-                                                    {4,5,6, 7,8,9,  1,2,3},
-                                                    {8,6,9, 1,2,3,  4,5,6}};
-
-    public static final int [][] INVALID_ROWS = {   {1,4,7, 1,4,7,  1,4,7},
-                                                    {2,5,8, 2,5,8,  2,5,8},
-                                                    {3,6,9, 3,6,9,  3,6,9},
-
-                                                    {4,7,1, 4,7,1,  4,7,1},
-                                                    {5,8,2, 5,8,2,  5,8,2},
-                                                    {6,9,3, 6,9,3,  6,9,3},
-
-                                                    {7,1,4, 7,1,4,  7,1,4},
-                                                    {8,2,5, 8,2,5,  8,2,5},
-                                                    {9,3,6, 9,3,6,  9,3,6}};
-
-    public static final int [][] INVALID_BLOCKS = { {1,2,3, 4,5,6,  7,8,9},
-                                                    {2,3,4, 5,6,7,  8,9,1},
-                                                    {3,4,5, 6,7,8,  9,1,2},
-
-                                                    {4,5,6, 7,8,9,  1,2,3},
-                                                    {5,6,7, 8,9,1,  2,3,4},
-                                                    {6,7,8, 9,1,2,  3,4,5},
-
-                                                    {7,8,9, 1,2,3,  4,5,6},
-                                                    {8,9,1, 2,3,4,  5,6,7},
-                                                    {9,1,2, 3,4,5,  6,7,8}};
+    public static final int[][] INVALID_BLOCKS = {  {1, 2, 3, 4, 5, 6, 7, 8, 9},
+                                                    {2, 3, 4, 5, 6, 7, 8, 9, 1},
+                                                    {3, 4, 5, 6, 7, 8, 9, 1, 2},
+                                                    {4, 5, 6, 7, 8, 9, 1, 2, 3},
+                                                    {5, 6, 7, 8, 9, 1, 2, 3, 4},
+                                                    {6, 7, 8, 9, 1, 2, 3, 4, 5},
+                                                    {7, 8, 9, 1, 2, 3, 4, 5, 6},
+                                                    {8, 9, 1, 2, 3, 4, 5, 6, 7},
+                                                    {9, 1, 2, 3, 4, 5, 6, 7, 8}};
 
     private ByteArrayOutputStream console;
 
@@ -82,35 +74,35 @@ class SudokuVerifierTest {
     }
 
     @Test
-    public void EndToEndSuppliedValidTest(){
+    public void EndToEndSuppliedValidTest() {
         SudokuVerifier sudokuVerifier = new SudokuVerifier(SudokuVerifierTest.VALID);
         sudokuVerifier.verifySolution();
         assertEquals("Solution is Valid", getStrippedConsole());
     }
 
     @Test
-    public void EndToEndSuppliedInvalidTest(){
+    public void EndToEndSuppliedInvalidTest() {
         SudokuVerifier sudokuVerifier = new SudokuVerifier(SudokuVerifierTest.INVALID);
         sudokuVerifier.verifySolution();
         assertEquals("Solution is Invalid", getStrippedConsole());
     }
 
     @Test
-    public void InvalidRowsReturnsInvalid(){
+    public void InvalidRowsReturnsInvalid() {
         SudokuVerifier sudokuVerifier = new SudokuVerifier(SudokuVerifierTest.INVALID_ROWS);
         sudokuVerifier.verifySolution();
         assertEquals("Solution is Invalid", getStrippedConsole());
     }
 
     @Test
-    public void InvalidColsReturnsInvalid(){
+    public void InvalidColsReturnsInvalid() {
         SudokuVerifier sudokuVerifier = new SudokuVerifier(SudokuVerifierTest.INVALID_COLS);
         sudokuVerifier.verifySolution();
         assertEquals("Solution is Invalid", getStrippedConsole());
     }
 
     @Test
-    public void InvalidBlocksReturnsInvalid(){
+    public void InvalidBlocksReturnsInvalid() {
         SudokuVerifier sudokuVerifier = new SudokuVerifier(SudokuVerifierTest.INVALID_BLOCKS);
         sudokuVerifier.verifySolution();
         assertEquals("Solution is Invalid", getStrippedConsole());
