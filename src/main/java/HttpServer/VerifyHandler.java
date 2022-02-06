68f28c4dbe8211ef4b0b24e630305b374db6c729
Mark Standen <mark@markstanden.dev>
2022-02-06 14:37:07 +0000

    working response

--- src/main/java/HttpServer/VerifyHandler.java
+++ src/main/java/HttpServer/VerifyHandler.java
@@ -3,7 +3,9 @@ package HttpServer;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 
@@ -12,10 +14,16 @@ public class VerifyHandler implements HttpHandler {
     /**
      * HttpHandler requires a handle function that takes a request.
      * TODO: parse POST request
+     *
      * @param exchange the encapsulated http request
      * @throws IOException
      */
     public void handle(HttpExchange exchange) throws IOException {
+        if (exchange.getRequestMethod().equals("POST")) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(exchange.getRequestBody()));
+            reader.lines()
+                    .forEach(System.out::println);
+        }
         String response = "<h1>Successfully reached page</h1>";
         exchange.sendResponseHeaders(HttpURLConnection.HTTP_OK, response.length());
         OutputStream responseBody = exchange.getResponseBody();
