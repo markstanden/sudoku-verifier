fa4ac1d802c9270f4654f974cce3ffc5f98809dd
Mark Standen <mark@markstanden.dev>
2022-02-11 04:55:21 +0000

    added html builder to further increase readability

--- src/main/java/SudokuHandler.java
+++ src/main/java/SudokuHandler.java
@@ -5,6 +5,8 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -32,13 +34,7 @@ public class SudokuHandler implements HttpHandler
 
 				String style = wrap(css, "style");
 				String body = wrap(title.concat(grid), "body");
-				response = ("<!DOCTYPE html>\n" +
-						"<html lang=\"en\">\n")
-						.concat(header)
-						.concat(style)
-						.concat(body)
-						.concat(footer)
-						.concat("<\\html>");
+				response = buildHtml(header,style, body, footer);
 			}
 			catch(FileNotFoundException e) {
 				System.out.println("An error occurred attempting to read the html file.");
@@ -69,5 +65,14 @@ public class SudokuHandler implements HttpHandler
 	private String wrap(String html, String tag)
 	{
 		return String.format("%n%n<%s>%n%s%n</%s>%n", tag, html.indent(4), tag);
+	}
+
+	private String buildHtml(String... components)
+	{
+		String docType = "<!DOCTYPE html>";
+		String openingTag = "<html lang=\"en\">";
+		String closingTag = "</html>";
+		String[] result = new String[]{docType, openingTag, String.join("\n", components), closingTag};
+		return String.join("\n", result);
 	}
 }
