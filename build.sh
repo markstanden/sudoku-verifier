c358a921c5a219fe8457562841763d5352be0fee
Mark Standen <mark@markstanden.dev>
2022-02-15 05:44:00 +0000

    missed - in network flag

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker build . --no-cache -t sudoku-verifier
 docker network create sudokuNetwork
-docker run -net=sudokuNetwork -p 5000:5000 sudoku-verifier
+docker run --net=sudokuNetwork -p 5000:5000 sudoku-verifier
